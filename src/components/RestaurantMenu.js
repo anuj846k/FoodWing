481d452a74b10d748f35a7ddcfd6fc69ef5cf17f
anuj846k <anuj846k@gmail.com>
2024-05-02 01:11:52 +0530

    Update CSS styles and add menu container

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import { MENU_API } from "../utils/constants";
+import { CDN_URL } from "../utils/constants";
+
 
 const RestaurantMenu = () => {
   const [resInfo, setResinfo] = useState(null);
@@ -14,38 +16,52 @@ const RestaurantMenu = () => {
 
   const fetchMenu = async () => {
     const data = await fetch(MENU_API + resId);
-
     const json = await data.json();
-    // console.log(json);
     setResinfo(json.data);
+    console.log(json);
   };
 
-  const { name, cuisines, costForTwoMessage } =
+  const { name, cuisines, costForTwoMessage, sla, avgRating } =
     resInfo?.cards[2]?.card?.card?.info || {};
 
   const { itemCards } =
     resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
       ?.card || {};
-  console.log(itemCards);
 
   return resInfo === null ? (
-    <Shimmer /> 
+    <Shimmer />
   ) : (
-    <div className="menu">
-      <h1>{name}</h1>
-      <h3>{cuisines.join(",")}</h3>
-      <h3>{costForTwoMessage}</h3>
-
-      <h2>Menu</h2>
-      <ul>
+    <div className="menu-container">
+      <div className="menu-home">
+        <div className="left">
+          <h1 className="restaurant-name">{name}</h1>
+          <h3>{avgRating} ratings</h3>
+          <h3 className="cuisine-info">{cuisines.join(", ")}</h3>
+        </div>
+        <div className="right">
+          <h3 className="cuisine-info">{costForTwoMessage}</h3>
+          <h4 className="delivery-time">{sla.deliveryTime} minutes</h4>
+        </div>
+      </div>
+      <h2 className="menu-heading">Menu</h2>
+      <div className="menu-item-list-container">
+    <ul className="menu-item-list">
         {itemCards &&
-          itemCards.map((item) => (
-            <li key={item.card.info.id}>
-              {item.card.info.name} - Rs.
-              {item.card.info.price / 100 ||item.card.info.defaultPrice/100}
-            </li>
-          ))}
-      </ul>
+            itemCards.map((item) => (
+                <li className="menu-item" key={item.card.info.id}>
+                    <div className="menu-item-details">
+                        <div className="menu-item-name-and-price">
+                            <span className="menu-item-name">{item.card.info.name}</span>
+                            <span className="menu-item-price">- Rs. {item.card.info.price / 100 || item.card.info.defaultPrice / 100}</span>
+                        </div>
+                        <span className ="item-description">{item.card.info.description}</span>
+                    </div>
+                    <img src={CDN_URL+item.card.info.imageId} alt="Image"/>
+                </li>
+            ))}
+    </ul>
+</div>
+
     </div>
   );
 };
