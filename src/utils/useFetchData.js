1429fa6b345a6f6a43e3b55d9ce1815b140c6d99
Anuj Kumar <anuj846k@gmail.com>
2024-06-11 00:15:47 +0530

    trying3 - removed .proxyrc

--- src/utils/useFetchData.js
+++ src/utils/useFetchData.js
@@ -6,42 +6,23 @@ const useFetchData = () => {
 
   //whenever state variables update, React triggers a reconciliation cycle (re-renders the component)
   console.log("body rendered");
-
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "/api/dapi/restaurants/list/v5?lat=28.5355161&lng=77.3910265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
-          {
-            method: 'GET',
-            credentials: 'include', // Ensure cookies and credentials are sent with the request
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const json = await response.json();
-        console.log(json);
-
-        setListofRes(
-          json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-        );
-        setFilteredRestaurant(
-          json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-        );
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
     fetchData();
   }, []);
 
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5355161&lng=77.3910265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setListofRes(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
   return { listofres, filteredRestaurant, setFilteredRestaurant };
 };
 
