b787a4a715a8409feeb9ffa54538ee18152a3b70
Anuj Kumar <anuj846k@gmail.com>
2024-06-10 11:23:28 +0530

    fetch updated

--- src/utils/useFetchData.js
+++ src/utils/useFetchData.js
@@ -16,7 +16,7 @@ const useFetchData = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5355161&lng=77.3910265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://swiggato-server.onrender.com/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5355161&lng=77.3910265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     console.log(json);
