88173bb2226beb082992f13adc84bb75e6512dfa
Anuj Kumar <anuj846k@gmail.com>
2024-06-18 20:28:57 +0530

    last try for swiggy api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,5 @@
-const CDN_URL = `${process.env.REACT_MEDIA_ASSETS_BASE_URL}image/upload/fl_lossy,f_auto,q_auto,w_660/`;
-const LOGO_URL =
-  "https://logowik.com/content/uploads/images/free-food-delivery699.logowik.com.webp";
-
-const Fetch_res = `${process.env.REACT_FOODFIRE_APP_BASE_URL}restaurants?lat=28.5355161&lng=77.3910265&page_type=DESKTOP_WEB_LISTING`;
-
-const MENU_API = `${process.env.REACT_FOODFIRE_APP_BASE_URL}menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5355161&lng=77.3910265&restaurantId=`;
-export { CDN_URL, LOGO_URL, MENU_API, Fetch_res };
+const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+const LOGO_URL = "https://logowik.com/content/uploads/images/free-food-delivery699.logowik.com.webp";
+const Fetch_res="https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5355161&lng=77.3910265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+const MENU_API="https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5355161&lng=77.3910265&restaurantId=";
+export { CDN_URL, LOGO_URL ,MENU_API,Fetch_res};
--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,9 +1,6 @@
-// CUSTOM HOOKS
 //Making a custom Hook so that we can follow Single Responsibility Principle and keep our code clean and modular.
 import { useEffect,useState } from "react";
 import { MENU_API } from "./constants";
-import axios from "axios";
-
 
 
 const useRestaurantMenu = (resId) => {
@@ -15,16 +12,14 @@ const useRestaurantMenu = (resId) => {
 
     const fetchMenu=async()=>{
         try {
-            const data=await axios.get(MENU_API+resId);
-            
-            setResinfo(data.data);
-            console.log(data.data)
+            const data=await fetch(MENU_API+resId);
+            const json=await data.json();
+            setResinfo(json.data);
+            console.log(json.data);
         } catch (error) {
             console.error(error.message);
         }
     }
     return resInfo;
-
 }
-
 export default useRestaurantMenu;
