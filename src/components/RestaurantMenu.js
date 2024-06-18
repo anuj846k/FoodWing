7a216e3938f5564d05ffb6720894621c1039c2d6
Anuj Kumar <anuj846k@gmail.com>
2024-06-18 21:50:24 +0530

    Deploying last stage

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -27,7 +27,7 @@ const RestaurantMenu = () => {
       ?.card || {};
 
   const categories =
-    resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (c) =>
         c.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
